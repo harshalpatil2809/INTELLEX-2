73896937801bf7e4c1d3dab78b0915b67ab3466c
harshalpatil2809 <harshalpatil28095@gmail.com>
2026-01-26 12:13:26 +0530

    Adjusting height of mobile

--- frontend/src/components/Chat/Chat.jsx
+++ frontend/src/components/Chat/Chat.jsx
@@ -4,11 +4,11 @@ const Chat = ({ message, setMessage }) => {
   console.log(message);
   return (
     <div className="flex flex-col flex-1 w-full items-center overflow-hidden">
-      <div className="flex-1 w-11/12 lg:max-w-3/4 overflow-y-auto overflow-x-hidden px-5 pt-3 ">
+      <div className="flex-1 w-11/12 lg:max-w-3/4 overflow-y-auto overflow-x-hidden px-5 pt-3  ">
         {message.map((msg, index) => (
           <div
             key={index}
-            className={`mb-2 font-mono wrap-break-word whitespace-pre-wrap leading-relaxed   ${
+            className={`mb-2 font-mono wrap-break-word whitespace-pre-wrap leading-relaxed ${
               msg.role === "user"
                 ? "text-center text-[#4F8CFF] w-fit bg-[#333] px-4 py-2 rounded-2xl "
                 : "text-center text-[#4ADE80] w-fit bg-[#333] px-4 py-2 rounded-2xl "
