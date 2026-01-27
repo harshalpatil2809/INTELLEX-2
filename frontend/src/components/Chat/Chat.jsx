936a8b25809dcf143506cf7c892fa6469958bb01
harshalpatil2809 <harshalpatil28095@gmail.com>
2026-01-27 09:11:42 +0530

    adjusting side of chats

--- frontend/src/components/Chat/Chat.jsx
+++ frontend/src/components/Chat/Chat.jsx
@@ -11,14 +11,14 @@ return (
       
     ) : (
       <>
-        <div className="flex-1 w-11/12 lg:max-w-3/4 overflow-y-auto overflow-x-hidden px-5 pt-3">
+        <div className="flex-1 w-[95vw] md:w-3/4 lg:w-3/4 items-center justify-center overflow-y-auto overflow-x-hidden px-5 pt-3">
           {message.map((msg, index) => (
             <div
               key={index}
-              className={`mb-2 font-mono wrap-break-word whitespace-pre-wrap leading-relaxed w-fit bg-[#333] px-4 py-2 rounded-2xl text-center ${
+              className={`mb-2 font-mono wrap-break-word whitespace-pre-wrap leading-relaxed w-full lg:w-fit md:w-fit px-4 py-2 rounded-2xl  ${
                 msg.role === "user"
-                  ? "text-[#4F8CFF]"
-                  : "text-[#4ADE80]"
+                  ? "text-[#4F8CFF] bg-transparent border border-blue-400 "
+                  : "text-[#4ADE80] bg-transparent border border-green-400 "
               }`}
             >
               {msg.text}
