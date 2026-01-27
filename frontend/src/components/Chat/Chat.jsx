c40d48a9b92bf294a6325f58770ced62036e5779
harshalpatil2809 <harshalpatil28095@gmail.com>
2026-01-27 18:32:02 +0530

    Loader adjusted

--- frontend/src/components/Chat/Chat.jsx
+++ frontend/src/components/Chat/Chat.jsx
@@ -1,44 +1,43 @@
 import Input from "../Input/Input.jsx";
-import {Loader} from "../Loader/Loader.jsx";
+import { Loader } from "../Loader/Loader.jsx";
 
 const Chat = ({ message, setMessage, loader, setLoader }) => {
-  console.log(message);    return (
-    <>
-return (
-  <div className="flex flex-col flex-1 w-full items-center overflow-hidden">
-    {loader ? (
-      <Loader />
-      
-    ) : (
-      <>
-        <div className="flex-1 w-[95vw] md:w-3/4 lg:w-3/4 items-center justify-center overflow-y-auto overflow-x-hidden px-5 pt-3">
+  return (
+    <div className="flex flex-col flex-1 w-full items-center justify-center overflow-hidden">
+      {loader ? (
+        <div className="z-100 "><Loader /></div>
+      ) : (
+        <>
+          {/* Messages */}
+          <div className="overflow-y-auto overflow-x-hidden flex flex-col items-center">
           {message.map((msg, index) => (
             <div
               key={index}
-              className={`mb-2 font-mono wrap-break-word whitespace-pre-wrap leading-relaxed w-full lg:w-fit md:w-fit px-4 py-2 rounded-2xl  ${
-                msg.role === "user"
-                  ? "text-[#4F8CFF] bg-transparent border border-blue-400 "
-                  : "text-[#4ADE80] bg-transparent border border-green-400 "
+              className={`flex w-[95vw] lg:w-3/4 md:w-3/4 mb-2  ${
+                msg.role === "user" ? "justify-start" : "justify-end"
               }`}
             >
-              {msg.text}
+              <div
+                className={`font-mono wrap-break-word whitespace-pre-wrap leading-relaxed px-4 py-2 rounded-2xl max-w-[75%] ${
+                  msg.role === "user"
+                    ? "text-white border "
+                    : "text-white border"
+                }`}
+              >
+                {msg.text}
+              </div>
             </div>
           ))}
-        </div>
+          </div>
 
-        <div className="w-full py-3 pb-10">
-          <Input setMessage={setMessage} setLoader={setLoader} />
-        </div>
-      </>
-    )}
-  </div>
-);
-
-
-    </>
+          {/* Input */}
+          <div className="w-full py-3 pb-10">
+            <Input setMessage={setMessage} setLoader={setLoader} />
+          </div>
+        </>
+      )}
+    </div>
   );
-  
-  
 };
 
 export default Chat;
